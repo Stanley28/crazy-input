f05d3cc6af2d8627e230a40829ae73a693846090
Stas <sv123.s@yandex.ru>
2020-07-19 12:56:34 +0300

    Added input component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Input from './components/Input.js';
+
+function App() {
+  return (
+    <div className='App'>
+      <Input id='input' />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Input.js
@@ -0,0 +1,152 @@
+import React from 'react';
+import '../styles/components/Input.css';
+
+export default class Input extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            active: false,
+            value: [],
+            caretPosition: 0,
+            isAllSelection: false
+        };
+        
+        this.inputRef = React.createRef();
+        
+        window.addEventListener('click', this.onClick.bind(this));
+        window.addEventListener('keypress', this.onKeyPress.bind(this));
+        window.addEventListener('keydown', this.onKeyDown.bind(this));
+    }
+
+    render() {
+        const active = this.state.active;
+
+        let value;
+        const caret = <span className='crazy-input__caret'></span>;
+        if (this.state.value.length === 0) {
+            if (active) {
+                value = caret;
+            } else {
+                value = '';
+            }
+        } else {
+            value = this.state.value.map((item, index) =>
+                <span className='crazy-input__value-item' key={index}>
+                    {this.state.caretPosition === 0 && this.state.caretPosition === index && active && caret}
+                    {item}
+                    {this.state.caretPosition - 1  === index && this.state.caretPosition !== 0 && active && caret}
+                </span>
+            );
+        }
+
+        let inputClasses = 'crazy-input';
+        inputClasses += active ? ' crazy-input_active' : '';
+        inputClasses += this.state.isAllSelection ? ' crazy-input_select' : '';
+        
+        return (
+            <div 
+                ref={this.inputRef} 
+                id={this.props.id} 
+                className={inputClasses}
+            >
+                <div class='crazy-input__value'>
+                    <span className='crazy-input__selection-overlay'></span>
+                    {value}   
+                </div>
+            </div>
+        );
+    }
+
+    onClick(event) {
+        if (this.inputRef.current !== event.target)  {
+            this.setState({
+                active: false,
+                isAllSelection: false
+            });
+            
+            return;
+        }      
+
+        event.stopPropagation();
+        
+        this.setState({
+            active: true
+        });
+    }
+
+    onKeyPress(event) {
+        if (!this.state.active) {
+            return;
+        }
+        
+        this.state.value.push(event.key)
+        this.setState({
+            caretPosition: ++this.state.caretPosition,
+            isAllSelection: false
+        });
+    }
+
+    onKeyDown(event) {
+        if (!this.state.active) {
+            return;
+        }
+
+        // backspace
+        if (event.keyCode === 8 && this.state.caretPosition > 0) {
+            if (this.state.isAllSelection) {
+                this.setState({
+                    value: [],
+                    caretPosition: 0,
+                    isAllSelection: false
+                });
+
+                return;
+            }
+
+            this.state.value.splice(this.state.caretPosition - 1, 1);
+            this.setState({
+                caretPosition: --this.state.caretPosition
+            });
+        }
+
+        // right
+        if (event.keyCode === 39) {
+            if (this.state.caretPosition < this.state.value.length) {
+                this.setState({
+                    caretPosition: ++this.state.caretPosition,
+                    isAllSelection: false
+                });
+            }
+        }
+
+        // left
+        if (event.keyCode === 37) {
+            if (this.state.caretPosition > 0) {
+                this.setState({
+                    caretPosition: --this.state.caretPosition,
+                    isAllSelection: false
+                });
+            }
+        }
+
+        // select
+        if (event.keyCode === 65 && event.ctrlKey) {
+            this.setState({
+                isAllSelection: true
+            });
+        }
+
+        // paste
+        if (event.keyCode === 86 && event.ctrlKey) {
+            navigator.clipboard.readText()
+                .then(text => {
+                    this.setState({
+                        value: [...this.state.value, ...text.split('')],
+                        caretPosition: this.state.value.length + text.length,
+                        isAllSelection: false
+                    })
+                })
+        }
+    }
+}
